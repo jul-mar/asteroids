977a84e2f0a29e097a5610851266ac84cd2e18a0
n0nikk <nolnmarten@gmail.com>
2024-10-23 22:49:03 +0200

    added Shots and shoot_timer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from shot import Shot
 
 def main():
     pygame.init()
@@ -8,9 +11,15 @@ def main():
     clock = pygame.time.Clock()
     updatable_group = pygame.sprite.Group()
     drawable_group = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable_group, drawable_group)
+    Asteroid.containers = (asteroids, updatable_group, drawable_group)
+    AsteroidField.containers = updatable_group
+    Shot.containers = (shots, updatable_group, drawable_group)
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
 
 
@@ -22,6 +31,10 @@ def main():
         screen.fill("black")
         for thing in updatable_group:
             thing.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
         for thing in drawable_group:
             thing.draw(screen)
         pygame.display.flip()
